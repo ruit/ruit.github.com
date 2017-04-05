fbc6c9e1da600b5e50e2652279d0e1e0dd674447
ruit <tianremi@gmail.com>
2017-04-05 17:38:25 +0800

    R

--- talks/2017training/Chapter2.R
+++ talks/2017training/Chapter2.R
@@ -20,14 +20,10 @@ summary(algae)
 hist(algae$mxPH, prob=T)
 
 
-library(car)
-par(mfrow=c(1,2))
 hist(algae$mxPH, prob=T, xlab='',
       main='Histogram of maximum pH value',ylim=0:1)
 lines(density(algae$mxPH,na.rm=T))
 rug(jitter(algae$mxPH))
-qq.plot(algae$mxPH,main='Normal QQ plot of maximum pH')
-par(mfrow=c(1,1))
 
 
 
@@ -35,23 +31,6 @@ par(mfrow=c(1,1))
 boxplot(algae$oPO4,ylab='Orthophosphate (oPO4)')
 rug(jitter(algae$oPO4),side=2)
 abline(h=mean(algae$oPO4,na.rm=T),lty=2)
-
-
-
-
-plot(algae$NH4,xlab='')
-abline(h=mean(algae$NH4,na.rm=T),lty=1)
-abline(h=mean(algae$NH4,na.rm=T)+sd(algae$NH4,na.rm=T),lty=2)
-abline(h=median(algae$NH4,na.rm=T),lty=3)
-identify(algae$NH4)
-
-
-plot(algae$NH4,xlab='')
-clicked.lines <- identify(algae$NH4)
-algae[clicked.lines,]
-
-
-algae[algae$NH4 > 19000,]
 
 
 library(lattice)
